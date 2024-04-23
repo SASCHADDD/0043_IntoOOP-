e88efb2480e887f1967e963f94f0d219c06b7212
SASCHADDD <saschadanu23@gmail.com>
2024-04-23 19:35:45 +0700

    correct

---
+++ OOP2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class bangunDatar
+{
+private:
+    float Luas;
+public:
+    float panja
+};
+
+bangunDatar::bangunDatar(/* args */)
+{
+}
+
+bangunDatar::~bangunDatar()
+{
+}
