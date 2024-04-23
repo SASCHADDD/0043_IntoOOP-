7342db5d9055868650ac900abc6329c9a6160a11
SASCHADDD <saschadanu23@gmail.com>
2024-04-23 18:52:14 +0700

    first commit

---
+++ OOP.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+   string nama;
+   int umur;
+   string
+};
+
+Mahasiswa(::Mahasiswa((/* args */)
+{
+}
+
+Mahasiswa(::~Mahasiswa(()
+{
+}
